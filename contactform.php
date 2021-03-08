6432946633180f7fa216d130992d30905d27923c
nholmstedt <60202951+nholmstedt@users.noreply.github.com>
2021-03-07 21:01:02 -0700

    Corrected url

--- contactform.php
+++ contactform.php
@@ -1,1 +1,16 @@
 <?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
+
+	$mailTo = "n.holmstedt@outlook.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+
+	mail($mailTo, $subject, $text, $headers);
+	header("Location: index.php?mailsend");
+}
+
