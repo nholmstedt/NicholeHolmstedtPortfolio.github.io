4e90e826f5cb8a8c7050579e3a2b6b31780ff453
nholmstedt <60202951+nholmstedt@users.noreply.github.com>
2021-03-08 12:56:29 -0700

    forms

---
+++ contact1.php
@@ -0,0 +1,8 @@
+<!-- Sample HTML Form -->
+<form class="contact-form" action="contactform.php" method="POST">
+      <input type="text" name="name" placeholder="Full Name">
+      <input type="text" name="email" placeholder="Your Email">
+      <input type="text" name="Subject" placeholder="Subject">
+      <textarea name="message" placeholder= "Message"></textarea>
+  <button type="submit" name="submit">Send Mail</button>
+</form>
